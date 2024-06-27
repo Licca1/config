ab11333f2675c419fb7386769c04fee4f30ebf3d
Licca <lIIllIIllI@protonmail.com>
2024-06-27 17:02:09 +0300

    Delete bri.sh

--- bri.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-actual=/sys/class/backlight/intel_backlight/actual_brightness;
-
-add_value=$1;
-
-current=$(cat $actual)
-
-echo $(($current + $add_value)) > /sys/class/backlight/intel_backlight/brightness;
