6348d2ba25089c6c18531099a627eb2c07e32fea
Licca <lIIllIIllI@protonmail.com>
2024-06-29 03:13:22 -0700

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -9,10 +9,14 @@ import XMonad.Hooks.StatusBar.PP
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
 import XMonad.Hooks.FadeInactive
+import XMonad.Layout.ThreeColumns 
+import XMonad.Layout.Renamed
+
 
 main :: IO ()
 main = xmonad $ ewmhFullscreen $ ewmh $ xmobarProp $ myConfig
 
+-- Win key
 myModMask     = mod4Mask
 
 myTerminal    = "kitty"
@@ -36,7 +40,6 @@ myConfig = def
 myKeys =
     [
       ( "M-<Space>",               spawn "dmenu_run")
-    , ( "M-S-<Space>",             spawn "dmenu_run")
     , ( "M-S-p",                   sendMessage $ JumpToLayout "Full")
     , ( "M-p",                     sendMessage NextLayout)
     , ( "M-S-l",                   spawn "xscreensaver-command -lock")
@@ -46,9 +49,24 @@ myKeys =
     , ( "<XF86AudioMicMute>",      spawn "pactl set-source-mute @DEFAULT_SOURCE@ toggle")
     , ( "<XF86MonBrightnessUp>",   spawn "lux -a 10%")
     , ( "<XF86MonBrightnessDown>", spawn "lux -s 10%")
+    , ( "M-f",                     spawn "firefox")
+    , ( "M-v",                     spawn "virt-manager")
     ]
 
-myLayoutHook = smartBorders . avoidStruts $ spacingWithEdge myPadding $ layoutHook def
+myLayout = tiled ||| threeColumns ||| Full 
+  where
+    threeColumns = named "Col" $ ThreeColMid nmaster delta ratio
+    tiled    = named "Tile" $ Tall nmaster delta ratio
+    nmaster  = 1      -- Default number of windows in the master pane
+    ratio    = 1/2    -- Default proportion of screen occupied by master pane
+    delta    = 3/100  -- Percent of screen to increment by when resizing panes
+
+mySpacing = spacingWithEdge
+
+myLayoutHook = renamed [CutWordsLeft 1] $
+  smartBorders . avoidStruts $
+  mySpacing myPadding $
+  myLayout 
 
 myManageHook = composeAll [ manageDocks,
                             isFullscreen --> doFullFloat,
