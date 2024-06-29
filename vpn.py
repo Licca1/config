8eb83eb540fd223b3e629ce3c6d85e99df42006d
Licca <lIIllIIllI@protonmail.com>
2024-06-29 03:14:22 -0700

    Update vpn.py

--- vpn.py
+++ vpn.py
@@ -7,18 +7,13 @@ def get_pia_value(key):
     lines = res.stdout.decode('ascii').split("\n")
     return lines[0]
 
-color = "cyan"
 is_connected = True if get_pia_value("connectionstate") == "Connected" else False
 
 if not is_connected:
-    print(f"<fc={color}></fc>")
+    print(f"")
 else:
     ip = get_pia_value("vpnip")
     region = get_pia_value("region")
     if ip == "Unknown":
-        color = "red"
         ip = "N/A"
-    print(f"(<fc={color}>{ip}</fc>:{region})")
-
-
-
+    print(f"{ip}")
